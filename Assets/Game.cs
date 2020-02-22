b162a342d2cdc2a89d90809c56600ac963b80d87
David Pomerenke <david.pomerenke@uni-konstanz.de>
2020-02-22 05:03:43 +0000

    Code sketch for Game class.

---
+++ Assets/Game.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Static class, for general game-play mechanisms / data.
+public class Game : MonoBehaviour
+{
+  ArrayList pile1 = new ArrayList<Card>();
+  ArrayList pile2 = new ArrayList<Card>();
+
+  void Start()
+  {
+    this.pile1 = this.pile1.shuffle();
+    this.pile2 = this.pile2.shuffle();
+  }
+
+  void Update()
+  {
+    // visual update of moves
+  }
+
+  void move(Player player)
+  {
+    player.position = player.position + this.diceRoll();
+    if (player.position >= Board.length)
+    {
+      player.position = player.position % Board.length;
+      Player.crossGo();
+
+    }
+    Board[player.position].landingAction(player);
+  }
+
+  void diceRoll(int repetition = 1)
+  {
+    Random rnd = new Random();
+    int dice1 = rnd.Next(1, 7);
+    int dice2 = rnd.Next(1, 7);
+    int result = dice1 + dice2;
+    maxRepetitions = 3;
+    if (dice1 == dice2)
+    {
+      Player.prisoned = false;
+      if (repetition <= maxRepetitions)
+      {
+        result = result + this.diceRoll(repetition + 1);
+      }
+    }
+  }
+
+}
