686bf2eae434f7d71d396b22fb8777a212780dd0
David Pomerenke <david.pomerenke@uni-konstanz.de>
2020-03-04 16:23:50 +0000

    remove gitignored stuff

---
+++ Board/Assets/Code/Tile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO: Make this an abstract class, create inheriting child classes for Go Tile, Street Tile, etc.
+public class Tile
+{
+  public int id;
+  public Tile(int id)
+  {
+    this.id = id;
+  }
+
+  public void landingAction(Player currentPlayer) {
+    Debug.Log("Player " + currentPlayer.id + "landed on tile " + this.id + ".");
+  }
+}
